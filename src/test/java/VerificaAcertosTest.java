204398d8838262060b4fb38811d2f8e80b7b6afd
Giulianobrjf <giuliano.leite2002@gmail.com>
2024-02-28 20:21:12 -0300

    refactor: comentarios retirados

--- src/test/java/VerificaAcertosTest.java
+++ src/test/java/VerificaAcertosTest.java
@@ -12,15 +12,13 @@ public class VerificaAcertosTest {
         ArrayList<Integer> numerosSorteados = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         VerificaAcertos verifica = new VerificaAcertos(numerosEscolhidos, numerosSorteados);
 
-        // Testando com um conjunto válido de números escolhidos
+
         assertTrue(verifica.getNumeroDeAcertos() > 0);
 
-        // Testando com um conjunto inválido de números escolhidos (tamanho fora do intervalo)
-        numerosEscolhidos = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)); // Tamanho maior que 15
+        numerosEscolhidos = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
         verifica = new VerificaAcertos(numerosEscolhidos, numerosSorteados);
         assertEquals(0, verifica.getNumeroDeAcertos().intValue());
 
-        // Testando com um conjunto inválido de números escolhidos (valores fora do intervalo)
         numerosEscolhidos = new ArrayList<>(Arrays.asList(0, 61, 62));
         verifica = new VerificaAcertos(numerosEscolhidos, numerosSorteados);
         assertEquals(0, verifica.getNumeroDeAcertos().intValue());
